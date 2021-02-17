690e1f2fccab5d80ccafc516da236731aeaf8177
xuexiangjys <xuexiangjys@163.com>
2021-02-17 18:29:00 +0800

    修复兼容问题

--- flutter_learn/android/app/src/main/kotlin/com/xuexiang/flutterLearn/MainActivity.kt
+++ flutter_learn/android/app/src/main/kotlin/com/xuexiang/flutterLearn/MainActivity.kt
@@ -4,20 +4,9 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import com.umeng.analytics.MobclickAgent
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
-
-  override fun onResume() {
-    super.onResume()
-    MobclickAgent.onResume(this)
-  }
-
-  override fun onPause() {
-    super.onPause()
-    MobclickAgent.onPause(this)
-  }
 }
